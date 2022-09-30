6fac3013b5b952ffd577741e8a836be5b14b3db8
rckivett <rckivett@gmail.com>
2022-09-29 20:09:16 -0500

    finished unit test studio

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +13,54 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
 
+    @Test
+    public void imbalancedOpenBracketsReturnsTrue() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void imbalancedClosedBracketsReturnsTrue() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+    @Test
+    public void fullNestReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+    @Test
+    public void imbalancedOpenCodeReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Code"));
+    }
+    @Test
+    public void imbalancedClosedCodeReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Code]"));
+    }
+    @Test
+    public void nestWithinWordReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+    @Test
+    public void balancedBracketsBeforeTextReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+    @Test
+    public void noBracketsReturnsTrue(){
+    assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+    @Test
+    public void closedBracketsBeforeOpenReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+    @Test
+    public void openCloseReverseOrderReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+    @Test
+    public void onlyOpenBracketReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
 }
